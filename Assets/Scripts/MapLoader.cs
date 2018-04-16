5609f14f4b1a9dc6896719ca291390c3cd3c4ed0
VoidX <voidxb@gmail.com>
2018-04-16 13:58:09 +0200

    Case-insensitive map loading

--- Assets/Scripts/MapLoader.cs
+++ Assets/Scripts/MapLoader.cs
@@ -20,10 +20,10 @@ namespace SketchMap {
             string[] Elements = File.ReadAllLines(Picked.FullName);
             foreach (string Element in Elements) {
                 string[] Split = Element.Split(' ');
-                if (Split[0].Equals("Wall"))
+                if (Split[0].ToLower().Equals("wall"))
                     Wall.CreateWall(Map, new Vector2(float.Parse(Split[1]), float.Parse(Split[2])), new Vector2(float.Parse(Split[3]), float.Parse(Split[4])),
                         new Color(float.Parse(Split[5]) / 255, float.Parse(Split[6]) / 255, float.Parse(Split[7]) / 255));
-                else if (Split[0].Equals("Gem"))
+                else if (Split[0].ToLower().Equals("gem"))
                     Gem.CreateGem(Map, new Vector2(float.Parse(Split[1]), float.Parse(Split[2])),
                         new Color(float.Parse(Split[3]) / 255, float.Parse(Split[4]) / 255, float.Parse(Split[5]) / 255));
             }
