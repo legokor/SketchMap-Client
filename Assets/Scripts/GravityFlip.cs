677684861efc2fe1b9bb5cf2600d519980b5dabe
VoidX <voidxb@gmail.com>
2018-04-19 02:35:46 +0200

    Option to flip gravity

---
+++ Assets/Scripts/GravityFlip.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace SketchMap {
+    public class GravityFlip : MonoBehaviour {
+        public void Flip() {
+            Physics.gravity = new Vector3(Physics.gravity.x, Physics.gravity.z, Physics.gravity.y);
+        }
+    }
+}
